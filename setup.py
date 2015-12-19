67f41e2f183a7db2818678e8049bf0125871b7d4
Gordon Bean <brazilbean@gmail.com>
2015-12-19 15:21:40 -0800

    created setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+from modulemagic import __version__
+
+setup(name='modulemagic',
+      version=__version__,
+      py_modules=['modulemagic'],
+      author='Gordon Bean',
+      author_email='brazilbean@gmail.com',
+      url='https://github.com/brazilbean/modulemagic')
